fbb968252e8535b2db791d34972372afce4e9510
zhuge <zhuge@noreply.gitcode.com>
2024-04-08 23:02:01 +0800

    update: 更新文件 高清MP4.js

--- js/高清MP4.js
+++ js/高清MP4.js
@@ -17,7 +17,7 @@ var rule = {
 	},
 	图片来源:'@Headers={"Accept":"*/*","Referer":"https://www.97tvs.com/","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"}',
 	timeout:5000,
-	class_parse: 'ul .menu&&li;a&&Text;a&&href;.*/(.*?).html',
+	class_parse: '.topnav ul li;a&&Text;a&&href;.*/(.*?).html',
   	//class_name: "动作片&科幻片&爱情片&喜剧片&剧情片&惊悚片&战争片&灾难片&罪案片&动画片&综艺&电视剧",
   	//class_url: "action&science&love&comedy&story&thriller&war&disaster&crime&cartoon&variety&sitcoms",
 	play_parse:true,

4e3377c0d857e012a9c4f71241c26194e3e845cb
zhuge <zhuge@noreply.gitcode.com>
2024-04-09 16:33:17 +0800

    upload: 上传文件 不太灵.js

---
+++ js/不太灵.js
@@ -0,0 +1,207 @@
+var rule = {
+	title:'不太灵',
+	host:'https://www.2bt0.com/',
+        homeUrl: '/',
+	url: '/movie/fyclass.html?sb=&sc=&sct=&scn=&sd=&sdt=&sdn=0&se=&set=&sen=0&sf=&sft=&sfn=&sg=1&sh=&sht=&shn=&page=fypage',
+	//https://www.2bt0.com/movie/fyclass.html?sb=&sc=&sct=&scn=&sd=&sdt=&sdn=0&se=&set=&sen=0&sf=&sft=&sfn=&sg=1&sh=&sht=&shn=&page=2
+	//filter_url:'{{fl.class}}',
+	filter:{
+	},
+	searchUrl: '/search.php?sb=**',
+	searchable:2,
+	quickSearch:0,
+	filterable:0,
+	headers:{
+		'User-Agent': 'PC_UA',
+         	'Cookie':''
+	},
+	timeout:5000,
+	class_name: '电影&电视剧',
+	class_url: '1&2',
+	play_parse:true,
+	play_json:[{
+		re:'*',
+		json:{
+			parse:0,
+			jx:0
+		}
+	}],
+	lazy:'',
+	limit:6,
+	推荐:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+let d = [];
+let html = request(input);
+let list = pdfa(html, 'ul.tabs-content li.active div');
+list.forEach(it => {
+	d.push({
+		title: pdfh(it, 'div.left h5&&Text'),
+		desc: pdfh(it, 'div.po_rel&&div.inblock&&Text'),
+		pic_url: /!'/.test(pd(it, '.po_abs&&style'))?pd(it, '.po_abs&&style'):pd(it, '.po_abs&&style').replaceAll("'",""),
+		url: pd(it, 'a&&href')
+	});
+});
+setResult(d);
+	`,
+	一级:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+let d = [];
+let html = request(input);
+let list = pdfa(html, 'div.masonry.masonry_container div');
+list.forEach(it => {
+	d.push({
+		title: pdfh(it, 'div.left h5&&Text'),
+		desc: pdfh(it, 'div.po_rel&&div.inblock&&Text'),
+		pic_url: /!'/.test(pd(it, '.po_abs&&style'))?pd(it, '.po_abs&&style'):pd(it, '.po_abs&&style').replaceAll("'",""),
+		url: pd(it, 'a&&href')
+	});
+});
+setResult(d);
+	`,
+	二级:{
+		title:"div.article-header h1&&Text",
+		img:"div.article-header div.pic img&&src",
+		desc:'div.article-header div.text&&Text',
+		content:'div.article-related.info p&&Text',
+		tabs:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+TABS=[]
+let d = pdfa(html, 'ul.down-list&&li a');
+let tabsa = [];
+let tabsq = [];
+let tabsm = false;
+let tabse = false;
+d.forEach(function(it) {
+	let burl = pdfh(it, 'a&&href');
+	if (burl.startsWith("https://www.aliyundrive.com/s/") || burl.startsWith("https://www.alipan.com/s/")){
+		tabsa.push("阿里雲盤");
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		tabsq.push("夸克網盤");
+	}else if (burl.startsWith("magnet")){
+		tabsm = true;
+	}else if (burl.startsWith("ed2k")){
+		tabse = true;
+	}
+});
+if (tabsm === true){
+	TABS.push("磁力");
+}
+if (tabse === true){
+	TABS.push("電驢");
+}
+if (false && tabsa.length + tabsq.length > 1){
+	TABS.push("選擇右側綫路");
+}
+let tmpIndex;
+tmpIndex=1;
+tabsa.forEach(function(it){
+	TABS.push(it + tmpIndex);
+	tmpIndex = tmpIndex + 1;
+});
+tmpIndex=1;
+tabsq.forEach(function(it){
+	TABS.push(it + tmpIndex);
+	tmpIndex = tmpIndex + 1;
+});
+log('mp4us TABS >>>>>>>>>>>>>>>>>>' + TABS);
+`,
+		lists:`js:
+log(TABS);
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+LISTS = [];
+let d = pdfa(html, 'ul.down-list&&li a');
+let lista = [];
+let listq = [];
+let listm = [];
+let liste = [];
+d.forEach(function(it){
+	let burl = pdfh(it, 'a&&href');
+	let title = pdfh(it, 'a&&Text');
+	log('dygang title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
+	log('dygang burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
+	let loopresult = title + '$' + burl;
+	if (burl.startsWith("https://www.aliyundrive.com/s/") || burl.startsWith("https://www.alipan.com/s/")){
+		if (true){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&url=" + encodeURIComponent(burl);
+		}
+		}else{
+			burl = "push://" + burl;
+		}
+		loopresult = title + '$' + burl;
+		lista.push(loopresult);
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		if (true){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&url=" + encodeURIComponent(burl);
+		}
+		}else{
+                        burl = "push://" + burl;
+                }
+		loopresult = title + '$' + burl;
+		listq.push(loopresult);
+	}else if (burl.startsWith("magnet")){
+		listm.push(loopresult);
+	}else if (burl.startsWith("ed2k")){
+		liste.push(loopresult);
+	}
+});
+if (listm.length>0){
+	LISTS.push(listm.reverse());
+}
+if (liste.length>0){
+	LISTS.push(liste.reverse());
+}
+if (false && lista.length + listq.length > 1){
+	LISTS.push(["選擇右側綫路，或3秒後自動跳過$http://127.0.0.1:10079/delay/"]);
+}
+lista.forEach(function(it){
+	LISTS.push([it]);
+});
+listq.forEach(function(it){
+	LISTS.push([it]);
+});
+`,
+
+	},
+	搜索:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+if (rule_fetch_params.headers.Cookie.startsWith("http")){
+	rule_fetch_params.headers.Cookie=fetch(rule_fetch_params.headers.Cookie);
+	let cookie = rule_fetch_params.headers.Cookie;
+	setItem(RULE_CK, cookie);
+};
+log('mp4us seach cookie>>>>>>>>>>>>>' + rule_fetch_params.headers.Cookie);
+let _fetch_params = JSON.parse(JSON.stringify(rule_fetch_params));
+//log("mp4us search params>>>>>>>>>>>>>>>" + JSON.stringify(_fetch_params));
+let search_html = request( HOST + '/search/' + encodeURIComponent(KEY) + '-1.html', _fetch_params)
+//log("mp4us search result>>>>>>>>>>>>>>>" + search_html);
+let d=[];
+//'div#list_all li;img.lazy&&alt;img.lazy&&src;div.text_info h2&&Text;a&&href;p.info&&Text',
+let dlist = pdfa(search_html, 'div#list_all li');
+dlist.forEach(function(it){
+	let title = pdfh(it, 'img.lazy&&alt');
+	if (title.includes(KEY)){
+		if (searchObj.quick === true){
+			title = KEY;
+		}
+		let img = pd(it, 'img.lazy&&src', HOST);
+		let content = pdfh(it, 'div.text_info h2&&Text');
+		let desc = pdfh(it, 'p.info&&Text');
+		let url = pd(it, 'a&&href', HOST);
+		d.push({
+			title:title,
+			img:img,
+			content:content,
+			desc:desc,
+			url:url
+			})
+	}
+});
+setResult(d);
+`,
+}

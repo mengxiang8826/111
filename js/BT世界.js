12668a450e7bb07aee57e5e31aa32648a590d48f
zhuge <zhuge@noreply.gitcode.com>
2024-04-12 18:41:33 +0800

    update: 更新文件 BT世界.js

--- js/BT世界.js
+++ js/BT世界.js
@@ -1,28 +1,33 @@
 var rule = {
-	title:'BT世界[磁]',
-	host:'https://www.btsj6.com/',
-	// host:'https://www.66s6.net',
-	// url: '/fyclass/index_fypage.html[/fyclass/index.html]',
-	url: '/category/fyclass/fypage/',
-	//filter_url:'{{fl.class}}',
+	title:'MP4电影[磁]',
+	host:'https://www.mp4us.com',
+        homeUrl: '/',
+	url: '/list/fyclass-fypage.html?',
+	filter_url:'{{fl.class}}',
 	filter:{
-		},
-	searchUrl: '/?s=**;GET',
-	//https://www.btsj6.com
+	},
+	searchUrl: '/search/**-1.html',
 	searchable:2,
 	quickSearch:0,
-	filterable:1,
+	filterable:0,
 	headers:{
-		'User-Agent': 'MOBILE_UA'
+		'User-Agent': 'PC_UA',
+         	'Cookie':''
 	},
 	timeout:5000,
     class_name: "动作片&科幻片&犯罪片&战争片&爰情片&悬疑片&喜剧片&恐怖片&灾难片&奇幻片&惊悚片&动画片&剧情片&纪录片&內地剧&欧美剧&韩剧&日剧",
   	class_url: "dongzuo&kehuan&fanzui&zhanzeng&aiqing&xuanyi&xiju&kongbu&zainan&qihuan&jingsong&donghua&juqing&jilu&neidi&oumei&hanju&riju",
-	//class_parse:''#nav&&li;a&&Text;a&&href;.*/(.*?).html',
-	cate_exclude:'',
 	play_parse:true,
+	play_json:[{
+		re:'*',
+		json:{
+			parse:0,
+			jx:0
+		}
+	}],
+	lazy:'',
 	limit:6,
-	推荐: `js:
+		推荐:`js:
 pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
 let d = [];
 let html = request(input);
@@ -52,43 +57,150 @@ list.forEach(it => {
 })
 setResult(d);
 `,
-		"title": "#content&&h1&&Text;.info_category&&Text",
-		"img": "#post_content&&img&&src",
-		"desc": ";;;#post_content&&p:eq(0)&&Text;#post_content&&p:eq(2)&&Text",
-		"content": "#post_content&&p:eq(1)&&Text",
-		"tabs": `js:
-			TABS = ["道长磁力"];
-			let tabs = pdfa(html, '#content&&h3:not(:contains(网盘))');
-			tabs.forEach((it) => {
-				TABS.push(pdfh(it, "body&&Text").replace('播放地址','道长在线').replace('（无插件 极速播放）','一').replace('（无需安装插件）','二'))
-			});
-		`,
-		"lists": `js:
-			log(TABS);
-			pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
-			LISTS = [];
-			let i = 1;
-			TABS.forEach(function(tab) {
-				if (/道长磁力/.test(tab)) {
-					var d = pdfa(html, '.context&&td');
-					d = d.map(function(it) {
-						var title = pdfh(it, 'a&&Text');
-						var burl = pd(it, 'a&&href');
-						return title + '$' + burl
-					});
-					LISTS.push(d)
-				} else if (/道长在线/.test(tab) && i <= TABS.length-1) {
-					var d = pdfa(html, '.context&&.widget:eq(list_idx)&&a'.replace("list_idx", i));
-					d = d.map(function(it) {
-						var title = pdfh(it, 'a&&Text');
-						var burl = pd(it, 'a&&href');
-						return title + '$' + burl
-					});
-					LISTS.push(d)
-					i = i + 1;
-				}
-			});
-		`,
+	二级:{
+		title:"div.article-header h1&&Text",
+		img:"div.article-header div.pic img&&src",
+		desc:'div.article-header div.text&&Text',
+		content:'div.article-related.info p&&Text',
+		tabs:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+TABS=[]
+let d = pdfa(html, 'ul.down-list&&li a');
+let tabsa = [];
+let tabsq = [];
+let tabsm = false;
+let tabse = false;
+d.forEach(function(it) {
+	let burl = pdfh(it, 'a&&href');
+	if (burl.startsWith("https://www.aliyundrive.com/s/") || burl.startsWith("https://www.alipan.com/s/")){
+		tabsa.push("阿里雲盤");
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		tabsq.push("夸克網盤");
+	}else if (burl.startsWith("magnet")){
+		tabsm = true;
+	}else if (burl.startsWith("ed2k")){
+		tabse = true;
+	}
+});
+if (tabsm === true){
+	TABS.push("磁力");
+}
+if (tabse === true){
+	TABS.push("電驢");
+}
+if (false && tabsa.length + tabsq.length > 1){
+	TABS.push("選擇右側綫路");
+}
+let tmpIndex;
+tmpIndex=1;
+tabsa.forEach(function(it){
+	TABS.push(it + tmpIndex);
+	tmpIndex = tmpIndex + 1;
+});
+tmpIndex=1;
+tabsq.forEach(function(it){
+	TABS.push(it + tmpIndex);
+	tmpIndex = tmpIndex + 1;
+});
+log('mp4us TABS >>>>>>>>>>>>>>>>>>' + TABS);
+`,
+		lists:`js:
+log(TABS);
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+LISTS = [];
+let d = pdfa(html, 'ul.down-list&&li a');
+let lista = [];
+let listq = [];
+let listm = [];
+let liste = [];
+d.forEach(function(it){
+	let burl = pdfh(it, 'a&&href');
+	let title = pdfh(it, 'a&&Text');
+	log('dygang title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
+	log('dygang burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
+	let loopresult = title + '$' + burl;
+	if (burl.startsWith("https://www.aliyundrive.com/s/") || burl.startsWith("https://www.alipan.com/s/")){
+		if (true){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&url=" + encodeURIComponent(burl);
+		}
+		}else{
+			burl = "push://" + burl;
+		}
+		loopresult = title + '$' + burl;
+		lista.push(loopresult);
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		if (true){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&url=" + encodeURIComponent(burl);
+		}
+		}else{
+                        burl = "push://" + burl;
+                }
+		loopresult = title + '$' + burl;
+		listq.push(loopresult);
+	}else if (burl.startsWith("magnet")){
+		listm.push(loopresult);
+	}else if (burl.startsWith("ed2k")){
+		liste.push(loopresult);
+	}
+});
+if (listm.length>0){
+	LISTS.push(listm.reverse());
+}
+if (liste.length>0){
+	LISTS.push(liste.reverse());
+}
+if (false && lista.length + listq.length > 1){
+	LISTS.push(["選擇右側綫路，或3秒後自動跳過$http://127.0.0.1:10079/delay/"]);
+}
+lista.forEach(function(it){
+	LISTS.push([it]);
+});
+listq.forEach(function(it){
+	LISTS.push([it]);
+});
+`,
+
 	},
-	搜索: '*',
+	搜索:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+if (rule_fetch_params.headers.Cookie.startsWith("http")){
+	rule_fetch_params.headers.Cookie=fetch(rule_fetch_params.headers.Cookie);
+	let cookie = rule_fetch_params.headers.Cookie;
+	setItem(RULE_CK, cookie);
+};
+log('mp4us seach cookie>>>>>>>>>>>>>' + rule_fetch_params.headers.Cookie);
+let _fetch_params = JSON.parse(JSON.stringify(rule_fetch_params));
+//log("mp4us search params>>>>>>>>>>>>>>>" + JSON.stringify(_fetch_params));
+let search_html = request( HOST + '/search/' + encodeURIComponent(KEY) + '-1.html', _fetch_params)
+//log("mp4us search result>>>>>>>>>>>>>>>" + search_html);
+let d=[];
+//'div#list_all li;img.lazy&&alt;img.lazy&&src;div.text_info h2&&Text;a&&href;p.info&&Text',
+let dlist = pdfa(search_html, 'div#list_all li');
+dlist.forEach(function(it){
+	let title = pdfh(it, 'img.lazy&&alt');
+	if (title.includes(KEY)){
+		if (searchObj.quick === true){
+			title = KEY;
+		}
+		let img = pd(it, 'img.lazy&&src', HOST);
+		let content = pdfh(it, 'div.text_info h2&&Text');
+		let desc = pdfh(it, 'p.info&&Text');
+		let url = pd(it, 'a&&href', HOST);
+		d.push({
+			title:title,
+			img:img,
+			content:content,
+			desc:desc,
+			url:url
+			})
+	}
+});
+setResult(d);
+`,
 }

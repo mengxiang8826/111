39aa9cc14e43e2f3e8f99632fafa86608987d35b
zhuge <zhuge@noreply.gitcode.com>
2024-04-14 19:25:36 +0800

    update: 更新文件 FREEOK.js

--- js/FREEOK.js
+++ js/FREEOK.js
@@ -23,7 +23,7 @@ var rule={
 	play_parse:true,
 	lazy:'',
 	limit:6,
-	推荐:'.module-items&&.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+	推荐:'.module-items&&a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
 	double:true, // 推荐内容是否双层定位
 	一级:'.module-items&&.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
 	二级:{

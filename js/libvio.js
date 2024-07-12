f5ffe1d47bdd49bd81695b6a49763d639d45f15c
zhuge <zhuge@noreply.gitcode.com>
2024-07-12 10:48:21 +0800

    update: 更新文件 libvio.js

--- js/libvio.js
+++ js/libvio.js
@@ -7,7 +7,7 @@ var rule = {
 	模板:'首图2',
 	// host:'https://tv.libvio.cc',
 	host:'https://libvio.app',
-	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(1)&&a:eq(1)&&href");print(src);HOST=src',
+	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(1)&&a&&href");print(src);HOST=src',
 	// url:'/type/fyclass-fypage.html',
 	url:'/show/fyclassfyfilter.html',
 	// url:'/show_fyclassfyfilter.html',
